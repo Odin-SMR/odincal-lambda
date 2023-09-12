f2ec89f8c620b680a6da8b6c9050e7d6e4c586dd
Andreas Skyman <andreas.skyman@molflow.com>
2023-09-12 15:22:29 +0200

    Implement new workflow

--- stacks/odincal_stack.py
+++ stacks/odincal_stack.py
@@ -5,11 +5,14 @@ from aws_cdk.aws_ec2 import Vpc, SubnetSelection, SubnetType
 from aws_cdk.aws_iam import Effect, PolicyStatement
 from aws_cdk.aws_lambda import (
     Architecture,
+    Code,
     DockerImageCode,
     DockerImageFunction,
+    Function,
+    InlineCode,
+    Runtime,
 )
 from aws_cdk.aws_s3 import Bucket
-from aws_cdk.aws_sqs import DeadLetterQueue, Queue
 from constructs import Construct
 
 
@@ -62,10 +65,66 @@ class OdincalStack(Stack):
             actions=["ssm:GetParameter"],
             resources=[f"arn:aws:ssm:*:*:parameter{ssm_root}/*"]
         ))
-        # TODO: Need lambdas
-        #   - DateInfo
-        #   - ScanInfo
-        #   - StartL2
+
+        date_info_lambda = Function(
+            self,
+            "OdinSMROdincalDateInfoLambda",
+            code=Code.from_asset(
+                "./cache_tables",
+                bundling={
+                    "image": Runtime.PYTHON_3_10.bundling_image,
+                    "command": [
+                        "bash",
+                        "-c",
+                        "pip install -r requirements.txt -t /asset-output && cp -au . /asset-output",
+                    ],
+                },
+            ),
+            handler="handler.date_info.handler",
+            timeout=lambda_timeout,
+            architecture=Architecture.X86_64,
+            runtime=Runtime.PYTHON_3_10,
+        )
+
+        scans_info_lambda = Function(
+            self,
+            "OdinSMROdincalScansInfoLambda",
+            code=Code.from_asset(
+                "./cache_tables",
+                bundling={
+                    "image": Runtime.PYTHON_3_10.bundling_image,
+                    "command": [
+                        "bash",
+                        "-c",
+                        "pip install -r requirements.txt -t /asset-output && cp -au . /asset-output",
+                    ],
+                },
+            ),
+            handler="handler.scans_info.handler",
+            timeout=lambda_timeout,
+            architecture=Architecture.X86_64,
+            runtime=Runtime.PYTHON_3_10,
+        )
+
+        activate_level2_lambda = Function(
+            self,
+            "OdinSMRLevel0Lambda",
+            code=InlineCode.from_asset("./activate_l2"),
+            handler="handler.activate_l2_handler.activate_l2_handler",
+            timeout=lambda_timeout,
+            architecture=Architecture.X86_64,
+            runtime=Runtime.PYTHON_3_10,
+        )
+
+        activate_level2_lambda.add_to_role_policy(
+            PolicyStatement(
+                actions=[
+                    "states:ListStateMachines",
+                    "states:StartExecution",
+                ],
+                resources=["*"],
+            ),
+        )
 
         # Set up tasks
         calibrate_level1_task = tasks.LambdaInvoke(
@@ -86,10 +145,40 @@ class OdincalStack(Stack):
             backoff_rate=2,
             interval=Duration.days(5),
         )
-        # TODO: Need tasks
-        #   - DateInfo
-        #   - ScansInfo
-        #   - StartL2
+        date_info_task = tasks.LambdaInvoke(
+            self,
+            "OdinSMROdincalDateInfoTask",
+            lambda_function=date_info_lambda,
+            payload=sfn.TaskInput.from_object(
+                {
+                    "Scans": sfn.JsonPath.string_at("$.CalibrateLevel1.Scans"),
+                },
+            ),
+            result_path="$.DateInfo",
+        )
+        scans_info_task = tasks.LambdaInvoke(
+            self,
+            "OdinSMROdincalScansInfoTask",
+            lambda_function=scans_info_lambda,
+            payload=sfn.TaskInput.from_object(
+                {
+                    "DateInfo": sfn.JsonPath.string_at("$.DateInfo.DateInfo"),
+                },
+            ),
+            result_path="$.ScansInfo",
+        )
+        activate_level2_task = tasks.LambdaInvoke(
+            self,
+            "OdinSMROdincalActivateLevel2Task",
+            lambda_function=scans_info_lambda,
+            payload=sfn.TaskInput.from_object(
+                {
+                    "Scans": sfn.JsonPath.string_at("$.ScansInfo.ScansInfo"),
+                    "File": sfn.JsonPath.string_at("$.name"),
+                },
+            ),
+            result_path="$.ActivateLevel2",
+        )
 
         # Set up workflow
         calibrate_level1_fail_state = sfn.Fail(
@@ -97,30 +186,86 @@ class OdincalStack(Stack):
             "OdinSMRCalibrateLevel1Fail",
             comment="Somthing went wrong when importing Level 0 file",
         )
-        calibrate_level1_success_state = sfn.Succeed(
-            self,
-            "OdinSMRCalibrateLevel1Success",
-        )
         check_calibrate_status_state = sfn.Choice(
             self,
             "OdinSMRCheckCalibrateLevel1Status",
         )
+
+        date_info_fail_state = sfn.Fail(
+            self,
+            "OdinSMRDateInfoFail",
+            comment="Somthing went wrong when updating Date Info tables",
+        )
+        check_date_info_status_state = sfn.Choice(
+            self,
+            "OdinSMRCheckDateInfoStatus",
+        )
+
+        scans_info_fail_state = sfn.Fail(
+            self,
+            "OdinSMRScansInfoFail",
+            comment="Somthing went wrong when updating Scans Info tables",
+        )
+        check_scans_info_status_state = sfn.Choice(
+            self,
+            "OdinSMRCheckScansInfoStatus",
+        )
+
+        activate_level2_fail_state = sfn.Fail(
+            self,
+            "OdinSMRActivateL2Fail",
+            comment="Somthing went wrong when calling Level2 processing",
+        )
+        activate_level2_success_state = sfn.Succeed(
+            self,
+            "OdinSMRActivateLevel2Success",
+        )
+        check_activate_level2_status_state = sfn.Choice(
+            self,
+            "OdinSMRActivateLevel2Status",
+        )
+
         check_calibrate_status_state.when(
-            # TODO: Look at StatusCode instead
-            sfn.Condition.boolean_equals(
-                "$.CalibrateLevel1.Payload.success",
-                True,
+            sfn.Condition.number_equals(
+                "$.CalibrateLevel1.Payload.StatusCode",
+                200,
             ),
-            calibrate_level1_success_state,
+            date_info_task,
         )
         check_calibrate_status_state.otherwise(calibrate_level1_fail_state)
         calibrate_level1_task.next(check_calibrate_status_state)
-        # TODO: Need steps etc. for
-        #   - DateInfo
-        #   - ScanInfo
-        #   - StartL2
 
-        sfn.StateMachine(
+        check_date_info_status_state.when(
+            sfn.Condition.number_equals(
+                "$.DateInfo.Payload.StatusCode",
+                200,
+            ),
+            scans_info_task,
+        )
+        check_date_info_status_state.otherwise(date_info_fail_state)
+        date_info_task.next(check_date_info_status_state)
+
+        check_scans_info_status_state.when(
+            sfn.Condition.number_equals(
+                "$.ScansInfo.Payload.StatusCode",
+                200,
+            ),
+            activate_level2_task,
+        )
+        check_scans_info_status_state.otherwise(scans_info_fail_state)
+        scans_info_task.next(check_scans_info_status_state)
+
+        check_activate_level2_status_state.when(
+            sfn.Condition.number_equals(
+                "$.AtcivateLevel2.Payload.StatusCode",
+                200,
+            ),
+            activate_level2_success_state,
+        )
+        check_activate_level2_status_state.otherwise(activate_level2_fail_state)
+        activate_level2_task.next(check_activate_level2_status_state)
+
+        fn.StateMachine(
             self,
             "OdinSMROdincalStateMachine",
             definition=calibrate_level1_task,
@@ -135,6 +280,5 @@ class OdincalStack(Stack):
         )
 
         psql_bucket.grant_read(calibrate_level1_lambda)
-        # TODO: Need to add same permission for
-        #   - DateInfo
-        #   - ScanInfo
+        psql_bucket.grant_read(date_info_lambda)
+        psql_bucket.grant_read(scans_info_lambda)
